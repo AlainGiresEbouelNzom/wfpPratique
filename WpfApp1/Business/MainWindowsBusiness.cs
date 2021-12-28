1f02cab2c5f1bac587408149d0c0937d4f8ed49e
ALAIN GIRES EBOUEL NZOM <egires@yahoo.fr>
2021-12-28 01:30:00 -0500

    Seance2FirstPart

---
+++ WpfApp1/Business/MainWindowsBusiness.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Business
+{
+    public class MainWindowsBusiness
+    {
+        public string TiltleOfMainWindow { get; set; }
+        public List<Models.Etudiant>  EtudiantList { get; set; }
+
+        public MainWindowsBusiness()
+        {
+            TiltleOfMainWindow = "Hello from the other side";
+            EtudiantList = new List<Models.Etudiant>();
+
+            EtudiantList.Add(new Models.Etudiant("Gires"));
+            EtudiantList.Add(new Models.Etudiant("Suzanne"));
+            EtudiantList.Add(new Models.Etudiant("Mireille"));
+            EtudiantList.Add(new Models.Etudiant("Edith"));
+            EtudiantList.Add(new Models.Etudiant("Chanel-A"));
+        }
+    }
+}
--- WpfApp1/Views/MainWindow.xaml.cs
+++ WpfApp1/Views/MainWindow.xaml.cs
@@ -1,18 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using WpfApp1.Models;
+﻿using System.Windows;
+using WpfApp1.Views.MyUserControls;
 
 namespace WpfApp1
 {
@@ -24,6 +11,21 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new Business.MainWindowsBusiness();
+        }
+
+
+
+        private void btnAjouter_Click(object sender, RoutedEventArgs e)
+        {
+            UCEtudiant uCE = new UCEtudiant();
+            grContent.Children.Add(uCE);
+
+        }
+
+        private void BtnEditerClick(object sender, RoutedEventArgs e)
+        {
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -31,19 +33,10 @@ namespace WpfApp1
 
         }
 
-        private void btnAjouter_Click(object sender, RoutedEventArgs e)
+        private void Button_ClickABsences(object sender, RoutedEventArgs e)
         {
-            List<Etudiant> EtudiantList = new List<Etudiant>();
-
-            Etudiant etudiant = new Etudiant("Gires Ebouel");
-
-            EtudiantList.Add(etudiant);
-            dgEtudiant.ItemsSource = EtudiantList;
-        }
-
-        private void BtnEditerClick(object sender, RoutedEventArgs e)
-        {
-
+            UCAbsence uCAbs = new UCAbsence ();
+            grContent.Children.Add(uCAbs);
         }
     }
 }
